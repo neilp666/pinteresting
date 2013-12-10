d3a7b724d9355e304453344b6300d6d7f5bcb1e4
neil patel <neil@myindianweddingapp.com>
2013-12-10 20:15:56 +0000

    add Amazon s3 for paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jbuilder', '~> 1.0.1'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
 gem 'devise', '~> 3.1.0.rc2'
 gem 'paperclip', '~> 3.0'
-gem 'aws-sdk', '~> 1.20.0'
+gem 'aws-sdk', '~> 1.24.0'
 
 
  group :development, :test do
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,44 +3,42 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
+
+
   def index
     @pins = Pin.all
   end
 
   def show
   end
- 
- 
+
   def new
     @pin = current_user.pins.build
   end
 
-  
   def edit
   end
 
-  
   def create
     @pin = current_user.pins.build(pin_params)
-      if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.' 
-      else
-        render action: 'new' 
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
-
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.' 
-      else
-        render action: 'edit' 
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
     end
-  
+  end
+
   def destroy
     @pin.destroy
-      redirect_to pins_url
+    redirect_to pins_url
   end
 
   private
@@ -50,8 +48,8 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-        @pin = current_user.pins.find_by(id: params[:id])
-        redirect_to pins_path, notice: "Not authorised to edit this pin" if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
