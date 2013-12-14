53f068a624bee2c12b314ca07b8032092b6cd0c3
neil patel <neil@myindianweddingapp.com>
2013-12-14 19:20:28 +0000

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
